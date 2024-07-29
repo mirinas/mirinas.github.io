c3fbf4ca1d5953a99d9e4b94cac99a712cb64386
mirinas <augustas.mirinas@gmail.com>
2024-07-29 23:42:07 +0300

    First deployment commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/css');
     eleventyConfig.addWatchTarget('src/css');
+    eleventyConfig.addPassthroughCopy('src/resources');
+    eleventyConfig.addWatchTarget('src/resources');
 
     return {
       dir: {
